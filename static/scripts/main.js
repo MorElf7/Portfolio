3cf8548322f657c4ba235c16be28fcf16bbe0746
MorElf7 <caothanhbang02@gmail.com>
2021-11-04 10:31:09 -0400

    main

---
+++ static/scripts/main.js
@@ -0,0 +1,15 @@
+document.getElementById("defaultOpen").click();
+
+function openProjects(evt, name) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("btn btn-secondary btn-lg");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(name).style.display = "block";
+    evt.currentTarget.className += " active";   
+}
