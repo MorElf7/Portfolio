8ef19371b09c481383646a5b40e793eb99c09793
MorElf7 <caothanhbang02@gmail.com>
2022-01-26 21:19:05 -0500

    update

--- project/views.py
+++ project/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, "projects/home.html")
+    return render(request, "project/home.html")
