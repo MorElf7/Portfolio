37ec3de6336ddb50f5fedf3f56868e27f2a9507d
Bang Cao <caothanhbang2002@gmail.com>
2023-04-17 01:06:25 -0400

    update resume page

--- cv/settings.py
+++ cv/settings.py
@@ -21,19 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-ytuoj%ytg!**jhdh%suuq*)7cfic&#namfu9vq5#+beup!c1(_'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-ytuoj%ytg!**jhdh%suuq*)7cfic&#namfu9vq5#+beup!c1(_'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 #'.herokuapp.com'
 
 # Application definition
 
 INSTALLED_APPS = [
-    'project',
+    # 'project',
     'resume',
     'django.contrib.admin',
     'django.contrib.auth',
